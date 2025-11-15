4188cfda22814f0b67c2efc640d322a4ef5fc7c4
PhoDafan <nhuy22174@gmail.com>
2025-11-15 19:18:21 +0700

    models view

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,39 @@
-from django.db import models
+from djongo import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Users(models.Model):
+    UserID = models.ObjectIdField(primary_key=True)
+    Username = models.CharField(max_length=50, unique=True)
+    PasswordHash = models.CharField(max_length=255)
+    Email = models.CharField(max_length=100, blank=True, null=True)
+    CreatedAt = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.Username
+
+
+class PredictionHistory(models.Model):
+    PredictionID = models.ObjectIdField(primary_key=True)
+    UserID = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+    StudyHoursPerWeek = models.FloatField()
+    PreviousGrade = models.FloatField()
+    AttendanceRate = models.FloatField()
+
+    ParentalSupport = models.CharField(
+        max_length=10,
+        choices=[
+            ("Low", "Low"),
+            ("Medium", "Medium"),
+            ("High", "High")
+        ]
+    )
+
+    ExtracurricularActivities = models.IntegerField()  # từ 1 đến 3
+    FinalGrade = models.FloatField()
+
+    PredictedAt = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.UserID.Username} → {self.FinalGrade}"
